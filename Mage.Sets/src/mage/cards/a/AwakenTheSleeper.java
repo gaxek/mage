efb463e4da35044834f457eab25ee9a22b396577
gaxek <sarah-michelle_krause@web.de>
2023-03-01 20:06:42 +0100

    Added Abilities to Awaken the Sleeper

--- Mage.Sets/src/mage/cards/a/AwakenTheSleeper.java
+++ Mage.Sets/src/mage/cards/a/AwakenTheSleeper.java
@@ -1,9 +1,18 @@
 package mage.cards.a;
 
 import java.util.UUID;
+
+import mage.abilities.effects.common.DestroyAllAttachedToTargetEffect;
+import mage.abilities.effects.common.UntapTargetEffect;
+import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.effects.common.continuous.GainControlTargetEffect;
+import mage.abilities.keyword.HasteAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.filter.StaticFilters;
+import mage.target.common.TargetCreaturePermanent;
 
 /**
  *
@@ -16,6 +25,11 @@ public final class AwakenTheSleeper extends CardImpl {
         
 
         // Gain control of target creature until end of turn. Untap that creature. It gains haste until end of turn. If it's equipped, you may destroy all Equipment attached to that creature.
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+        this.getSpellAbility().addEffect(new GainControlTargetEffect(Duration.EndOfTurn));
+        this.getSpellAbility().addEffect(new UntapTargetEffect().setText("Untap that creature"));
+        this.getSpellAbility().addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn).setText("It gains haste until end of turn."));
+        this.getSpellAbility().addEffect(new DestroyAllAttachedToTargetEffect(StaticFilters.FILTER_PERMANENT_EQUIPMENT, "that creature"));
     }
 
     private AwakenTheSleeper(final AwakenTheSleeper card) {
